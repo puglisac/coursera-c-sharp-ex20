2b1f582cafea2ee134477dc823f6bebf5a3909cc
puglisac <alan.c.puglisi@gmail.com>
2020-12-17 12:40:25 -0600

    added keyboard input

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -11,6 +11,18 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 position = transform.position;
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        if(horizontalAxis != 0)
+        {
+            position.x += horizontalAxis * MoveUnitsPerSecond * Time.deltaTime;
+        }
+        if (verticalAxis != 0)
+        {
+            position.y += horizontalAxis * MoveUnitsPerSecond * Time.deltaTime;
+        }
+        transform.position = position;
     }
 }
